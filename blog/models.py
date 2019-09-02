afd166f3cd444331553f4096d2ad6e8f9b79fcba
Chandrashekhar9 <ace.cs99@gmail.com>
2019-09-02 12:30:54 +0530

    Maked database for blog post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=50)
+    images = models.ImageField(upload_to='images/blog/')
+    article = models.TextField()
+    pub_date = models.DateTimeField()
 
 
 
